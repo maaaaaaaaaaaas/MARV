ce18831fa5fd0b018887cf58d93e0e1368d4d49f
mask <131876904+ss-mask-ss@users.noreply.github.com>
2024-02-10 00:47:02 +0900

    Add files via upload

---
+++ Marv2/config.py
@@ -0,0 +1,13 @@
+# config.py
+
+settings = {
+    'token': 'MTEzMDg2NzIxOTE5NTI0MDUzOA.Ge8Dem.MbsTBQEkhye0GOxCodSb0VC-7hvrKcWn6fZ19U',
+    'prefix': 'm!',
+    'path_to_ffmpeg': 'C:\FFmpeg\\bin\\ffmpeg.exe' , # または 'C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe' など
+    'emojis': {
+        'wuuut': 518051242807787520,
+        'youtube': 878537811601555466,
+        'stonks': 879411306157985862,
+        'squid_cleaning': 880326444356612116
+    },
+}
